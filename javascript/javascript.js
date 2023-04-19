9fec27bd4cc2f3b50b6d2e4c6811fc1031b65886
Shraddhajain950 <sjain83@id.iit.edu>
2023-04-19 05:18:28 -0500

    trying js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,15 @@
-var map;
-function initMap() {
-  map=new googl.maps.Map(document.getElementById('map'){
-    center: {lat; -34.397, lng:150.644}
+let map;
+
+async function initMap() {
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
   });
 }
+
+initMap();
+
+
