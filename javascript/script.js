1bc1c50eef998112386760ddb3b7968e30e061a1
Shraddhajain950 <sjain83@id.iit.edu>
2023-04-24 22:48:27 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,7 @@
 let map; 
 
-const kaplanLatLong = {lat: 41.83711715348835, lng: -87.62830738921376}; 
+const kaplanLatLong = {lat: 41.83711715348835, lng: -87.62830738921376};
+const IIT_Tower = {lat:41.831617715598576,lng: -87.62721510868744 }
 function initMap() {
   const kaplanLatLong = { lat: 41.83711715348835, lng: -87.62830738921376}; 
   map = new google.maps.Map(document.getElementById("map"), 
@@ -9,6 +10,23 @@ function initMap() {
    position: kaplanLatLong,
   map:map,});
 } 
+function AddMarkerToKaplan(){
+  const marker = new google.maps.Marker({
+    position: IIT_Tower,
+	map:map,
+  });
+
+
+const georssLayer = new google.maps.KmlLayer({
+    url: "http://api.flickr.com/services/feeds/geo/?g=322338@N20&lang=en-us&format=feed-georss",
+  });
+
+  georssLayer.setMap(map);
+  
+}
+
+
+
 
 
 
