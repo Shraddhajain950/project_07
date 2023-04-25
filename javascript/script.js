dfe649b52c0c066c4e01d11b2cbab83dfbd63b9b
Shraddhajain950 <sjain83@id.iit.edu>
2023-04-24 20:18:56 -0500

    adding markers

--- javascript/script.js
+++ javascript/script.js
@@ -4,4 +4,11 @@ const kaplanLatLong = {lat: 41.83711715348835, lng: -87.62830738921376};
 function initMap() {
   const kaplanLatLong = { lat: 41.83711715348835, lng: -87.62830738921376}; 
   map = new google.maps.Map(document.getElementById("map"), 
-  {center: kaplanLatLong,  zoom: 5,}); } 
+  {center: kaplanLatLong,  zoom: 12,}); 
+  const marker= new google.maps.Marker ({
+   position: kaplanLatLong,
+  map:map,});
+} 
+
+
+
